ac7ef906318312d550403330351fdc3f60d71b4a
Shariar Kabir <shariar.k8@gmail.com>
2018-09-21 20:53:56 -0400

    fix issue with incorrect postgresql link

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "mysecretkey"
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgres://ycfwfrua:u1u7FGchZENVMu6smfV5dW_B67c_35sM@elmer.db.elephantsql.com:5432/ycfwfrua"
 
 class TestingConfig(Config):
     TESTING = True
