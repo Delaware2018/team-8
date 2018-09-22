8514ef86c980854d53faf03309d9ec801a4298c0
Shariar Kabir <shariar.k8@gmail.com>
2018-09-21 21:24:27 -0400

    changed run configuration to production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
