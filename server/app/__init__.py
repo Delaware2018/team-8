c3559d20d859efcd8a33eca7de336244c801fa38
Shariar Kabir <shariar.k8@gmail.com>
2018-09-22 00:59:38 -0400

    /groups/ end point now creates a new group entry if correct params are given

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,8 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
+from flask_restful import Resource, reqparse
+
 
 from instance.config import app_config
 
@@ -7,12 +10,34 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    from app.models import Group
+
+    @app.route('/groups/', methods=['POST', 'GET'])
+    def groups():
+        if request.method == "POST":
+            name = request.args['name']
+            donated = float(request.args['donated'])
+            if name:
+                group = Group(name = name)
+                group.donated = donated
+                group.save()
+                response = jsonify({
+                    'id': group.id,
+                    'name': group.name,
+                    'donated': group.donated
+                })
+                response.status_code = 201
+            return response
+        else:
+            # GET
+            return "Get"
 
     @app.route('/', methods=['GET'])
     def main():
