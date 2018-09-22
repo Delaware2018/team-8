285a82ccf27b0d5c22d95142cbd7224cede5cbaf
Shariar Kabir <shariar.k8@gmail.com>
2018-09-22 01:56:11 -0400

    added GroupUsers save and delete method

--- server/app/models.py
+++ server/app/models.py
@@ -60,5 +60,13 @@ class GroupUser(db.Model):
     def __init__(self, name):
         self.name = name
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def __repr__(self):
         return "<Group: {}>".format(self.name)
